463cdad530e71a16c873f21b363eb6818715ecfa
Wilson Chiviti <wilsonchiviti@gmail.com>
2023-02-12 21:09:40 +0200

    Setting up maven project reactor pattern. DDD modular implementation Setting up spring boot application. Initial commit.

---
+++ app/src/main/java/io/github/hobbstech/DronesApplication.java
@@ -0,0 +1,11 @@
+package io.github.hobbstech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DronesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DronesApplication.class, args);
+    }
+}
